66dc662d271890b81869e2c044acc8c6350eecca
Tienstra <msz.tienstra@gmail.com>
2025-02-11 12:51:31 +0100

    Added Sample function

--- free_energy_barries/utils/tools.py
+++ free_energy_barries/utils/tools.py
@@ -9,15 +9,20 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.spherical_coords import sample_spherical_coords, spherical_to_cartesian
 
 
-def sample_annulai():
-    # Parameters
-    n_dim = 3  # Number of dimensions
-    n_samples = 10000  # Number of samples
-    r_low = 0.9  # Lower bound for radial coordinate
-    r_high = 1.0  # Upper bound for radial coordinate
+def sample_annulai(n_dims=3, n_samples=5000, r_low=.66, r_high=1):
+    """
+    Samples from the annulai with specified radius
 
+    Parameters: 
+        - n_dims : dimension of samples 
+        - n_samples : number of samples to be drawn
+        - r_low : lower bound for radius (inner radius)
+        - r_high: upper bound for radius (outer radius)
+    """
     # Generate samples
     r, phis = sample_spherical_coords(n_dim, n_samples, r_low, r_high)
 
     # Convert to Cartesian coordinates
-    x = spherical_to_cartesian(r, phis)
+    samples = spherical_to_cartesian(r, phis)
+
+    return samples 

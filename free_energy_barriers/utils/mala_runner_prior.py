corrected prior variance

free_energy_barriers/utils/mala_runner_prior.py
@@ -45,8 +45,9 @@ def run_mala_experiments(dim, n_steps=1000):
 
     # Initialize model and sampler
     model = LogisticRegression(N=n_samples, p=n_features)
+    sigma_prior = 1/jnp.sqrt(config.D)
     log_posterior_fn = create_log_posterior(
-        model, X_jax, y_jax, sigma_prior=1.0
+        model, X_jax, y_jax, sigma_prior
     )
     # Initialize kernel
     mala_kernel = MALAKernel(log_posterior_fn, epsilon=config.epsilon)